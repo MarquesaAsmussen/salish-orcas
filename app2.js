b2c7b23f40b8e821df5ec21ad81f78a75df3ce85
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-07-05 14:13:16 -0700

    updating with js apps

---
+++ app2.js
@@ -0,0 +1,6 @@
+let likeOrcas = prompt('Do you like orcas?');
+if(likeOrcas == 'yes'){
+    document.write('Wonderful, we do too!')
+} else {
+    document.write("That's so sad!")
+}
---
+++ app3.js
@@ -0,0 +1,6 @@
+let recieveUpdates = prompt('Would you like to recieve updates from Salish Orcas?');
+if(recieveUpdates == 'sure'){
+    document.write('Please call us to setup an account')
+} else {
+    document.write('Ok')
+}
