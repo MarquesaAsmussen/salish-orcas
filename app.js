b7c858bae8bad624b13db555034c82118b63b9ae
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-07-05 20:32:11 -0700

    testing app.js consolidation

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ function getUserName(){
 
 let userName = getUserName()
 console.log(userName)
+
+function doYouLikeOrcas(){
+    let likeOrcas = prompt('Do you like orcas?');
+    if(likeOrcas == 'yes'){
+        document.write('Wonderful, we do too!')
+    } else {
+        document.write("That's so sad!")
+    }
+}
+
+doYouLikeOrcas();
+
+function wantToGetUpdates(){
+    let receiveUpdates = prompt('Would you like to receive updates from Salish Orcas?');
+    if(receiveUpdates == 'yes'){
+        document.write('Please call us to setup an account')
+    } else {
+        document.write('Ok')
+    }
+}
+
+wantToGetUpdates();
