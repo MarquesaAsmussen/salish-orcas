e1da86bc7b8cba4a05b84f245cd3ad0d5010342f
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-07-05 21:14:01 -0700

    update with user guessing game

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ function doYouLikeOrcas(){
     }
 }
 
-doYouLikeOrcas();
-
 function wantToGetUpdates(){
     let receiveUpdates = prompt('Would you like to receive updates from Salish Orcas?');
     if(receiveUpdates == 'yes'){
@@ -27,4 +25,26 @@ function wantToGetUpdates(){
     }
 }
 
-wantToGetUpdates();
+function guessingGame(){
+    let correctAnswer = 42;
+    let numberOfGuesses = 5;
+    for(let i = 0; i < numberOfGuesses; i++){
+        console.log(i);
+        let userAnswer = prompt('Please enter a number between 1 - 100');
+        while(userAnswer <= 0 || userAnswer > 100){
+            userAnswer = prompt('Please enter a number between 1 - 100');
+            console.log('User Guess: ' + userAnswer)
+        }
+        document.write('<h4>'+userAnswer+'</h4>')
+        if(userAnswer == correctAnswer){
+            alert('Ah, yes, 42. The answer to Life, the Universe, and Everything. Well done, you nerd!');
+            break;
+        } else if (userAnswer < correctAnswer){
+            alert('little bit higher, now.');
+        }else if (userAnswer > correctAnswer){
+            alert('little bit lower, now');
+        } else {
+            alert('Hmm, something is not right!');
+        }
+        }
+    }
